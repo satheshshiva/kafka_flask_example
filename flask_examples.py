32d2e297310a97f2bd0773dff064b416edfe5c3a
Sathesh Sivashanmugam <satheshshiva@gmail.com>
2024-12-27 11:17:09 -0700

    flask examples for post and json

--- flask_examples.py
+++ flask_examples.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, json
 
 app = Flask(__name__)
 
@@ -17,13 +17,13 @@ def show_user_profile(username):
 @app.route('/id/<int:id>')
 def show_post(id):
     # show the post with the given id, the id is an integer
-    return "ID is %d" % post_id
+    return "ID is %d" % id
 
 #Return HTML example
 @app.route('/idInHtml/<int:id>')
 def idInHtml(id):
     # show the post with the given id, the id is an integer
-    return "ID is <b>%d</b>" % post_id
+    return "ID is <b>%d</b>" % id
 
 #Query Parameter Example
 @app.route('/queryparam_example')
@@ -36,6 +36,10 @@ def queryparam_example():
 def login():
     if request.method == 'POST':
         param1 = request.args.get('param1') 
-        return "POST Method"
+        try:
+            jsonData = json.loads(request.data)
+        except:
+            return "You sent param1 as {}. No json data was sent" % param1
+        return "You sent param1 as <b>{}</b>. JSON you sent is <b>{}</b>. I am able to parse the name attribute as <b>{}</b>".format(param1,jsonData, jsonData["name"])
     else:
         return "This route only support POST method"
